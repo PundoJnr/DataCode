64d97860a100226087172e2189c492a40cd82165
ArthurPundo <pundoarthur@gmail.com>
2018-03-15 23:02:56 +0300

    successful database entry

--- upload.php
+++ upload.php
@@ -1,62 +1,31 @@
 <?php
 
-if(isset($_POST['submit']))
-{
-    $name=$_POST['name'];
-    $id=$_POST['id'];
-    $email=$_POST['email'];
-    $pasword=$_POST['psswd'];
-    $password=$_POST['pswd'];
 
-$target_Folder = "img/";
+  
 
-$uid = $_POST['id'];
+        
+        //connect
+        $con = mysqli_connect("localhost", "root", "", "data");
+        //check connection
+        if($con === false){
+            die("Couldnt connect." . mysqli_connect_error());
+        }else{
+            echo"successful connection";
+        }
+        $names = mysqli_real_escape_string($con, $_REQUEST['name']);
+        $id = mysqli_real_escape_string($con, $_REQUEST['id']);
+        $email = mysqli_real_escape_string($con, $_REQUEST['email']);
+        $password = mysqli_real_escape_string($con,$_REQUEST ['psswd']);
+        
+        $sql = "INSERT INTO users (names,id,email,pasword)
+                    VALUES('$names','$id','$email','$password')";
 
-$target_Path = $target_Folder.basename( $_FILES['uploadimage']['name'] );
-
-$savepath = $target_Path.basename( $_FILES['uploadimage']['name'] );
-
-    $file_name = $_FILES['uploadimage']['name'];
-
-    if(file_exists('upload/'.$file_name))
-{
-    echo "That File Already Exisit";
-    }
-    else
-    {
-
-        // Database 
-    $con=mysqli_connect("localhost","root","","data"); //Change it if required
-
-//Check Connection
-        if(mysqli_connect_errno())
-        {
-            echo "Failed to connect to database" .     mysqli_connect_errno();
+        if(mysqli_query($con,$sql)){
+            echo "Successful Registration";
+        }else{
+            echo"Couldnt not add record" . mysqli_error($con);
         }
 
-        $sql = "INSERT INTO users (id,name,email,password,image, image_name)
-                    VALUES     ('$id','$name','$email','$password','$target_Folder$file_name','$file_name') ";
-    }
-        if (!mysqli_query($con,$sql))
-        {
-            die('Error: ' . mysqli_error($con));
-        }
-        echo "1 record added successfully in the database";
-        echo '<br />';
-        mysqli_close($con);
 
-        // Move the file into UPLOAD folder
-
-        move_uploaded_file( $_FILES['uploadimage']['tmp_name'],     $target_Path );
-
-        echo "File Uploaded <br />";
-        echo 'File Successfully Uploaded to:&nbsp;' . $target_Path;
-        echo '<br />';  
-        echo 'File Name:&nbsp;' . $_FILES['uploadimage']['name'];
-        echo'<br />';
-        echo 'File Type:&nbsp;' . $_FILES['uploadimage']['type'];
-        echo'<br />';
-        echo 'File Size:&nbsp;' . $_FILES['uploadimage']['size'];
-
-    }
+    
 ?>
