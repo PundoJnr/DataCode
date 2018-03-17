a95fc88c44c40c499c74a4f974dd02063136f3cc
ArthurPundo <pundoarthur@gmail.com>
2018-03-17 14:04:01 +0300

    getting left

--- success.php
+++ success.php
@@ -2,15 +2,19 @@
     //connect
     $conn = mysqli_connect('localhost','root','','data');
 
-    //from html
+    if($conn == false){
+        die ("no connection" .mysql_connect_error());
+    }
 
     $id = mysqli_real_escape_string($conn, $_REQUEST['id']);
 
     //get data
-    $sql = "SELECT * FROM users WHERE id == '".$id."'";
-    $data = mysqli_query($conn,$sql) or die('error!');  
-
-
-  
+    $sql = "SELECT * FROM users WHERE Id_number== '".$id."";
+    $data = mysqli_query($conn,$sql);
     
+    if($data){
+        echo"<input type='text' value='$name' name='name'>";
+    }else{
+        echo("can't" . mysql_error());
+    }   
 ?>
