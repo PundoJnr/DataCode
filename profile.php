8b9c09ad5bd772120780ddde58e089e49772b267
ArthurPundo <pundoarthur@gmail.com>
2018-03-17 12:15:39 +0300

    initial commit

---
+++ profile.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title></title>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    </head>
+    <body class="container">
+            <nav style="background:purple;" class="navbar ">
+                    <a class="navbar-brand" href="home.html" style="color:orange;">Data-Code</a>
+                  </nav>
+
+<div class="container">
+        <div class="row text-center">
+           <div>
+
+           </div>
+            <div class="col-sm-4 col-md-12">
+                <blockquote>
+                    <p>Bruce Wayne</p> <small><cite title="Source Title">Gotham, United Kingdom  <i class="glyphicon glyphicon-map-marker"></i></cite></small>
+                </blockquote>
+                <p> <i class="glyphicon glyphicon-envelope"></i> masterwayne@batman.com
+                    <br
+                    /> <i class="glyphicon glyphicon-globe"></i> www.bootsnipp.com
+                    <br /> <i class="glyphicon glyphicon-gift"></i> January 30, 1974</p>
+            </div>
+            
+     
+        </div>
+    </div>
+</form>
+
+<footer id="footer" class="navbar container " style="background-color: purple;">
+    <div style="color:orange; position: fixed;Bottom:0px;"> &copy; 2018 </div>
+</footer>
+    </body>
+
+</html>
